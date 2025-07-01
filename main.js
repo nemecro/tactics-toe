97edbba6d26d572b229a603f259e145592e86074
nemecro <nemecstudies@gmail.com>
2025-07-01 18:08:23 +0200

    create a gameboard object using IIFE pattern in order to block unwanted access to the board array

--- main.js
+++ main.js
@@ -42,11 +42,36 @@ function checkVictory(board) {
     return 0;
 }
 
-const gameboard = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-];
+// const gameboard = [
+    // [0,0,0],
+    // [0,0,0],
+    // [0,0,0]
+// ];
+
+const gameboard = function(){
+    let board = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
+
+    const getState = function(){
+        return board.map(row => row.slice());
+    }
+
+    const setField = function(row, col, symbol){
+        board[row][col] = symbol;
+    }
+
+    const reset = function(){
+        board = board.map(row => Array(row.length).fill(0));
+    }
+
+    return {
+        getState, setField, reset
+    }
+
+}();
 
 const createPlayer = function(name, symbol){
     return {
@@ -56,35 +81,36 @@ const createPlayer = function(name, symbol){
 }
 
 const player1 = createPlayer('Roland', 'X');
-
 const player2 = createPlayer('Olga', '0');
 
-let gameStatus = -1;
-let activePlayer = player1;
+function playGame(){
+    let gameStatus = -1;
+    let activePlayer = player1;
+    let row = 0;
+    let column = 0;
+    
+ 
+    while(gameStatus === -1){
+        activePlayer = activePlayer === player1 ? player2 : player1;
+        alert(`${activePlayer.name}'s turn`);
 
-let row = 0;
-let column = 0;
+        do {
+            row = prompt('Row: ');
+            column = prompt('Column: ');
+        } while (gameboard[row] == undefined || gameboard[row][column] == undefined || gameboard[row][column] !== 0)
 
-while(gameStatus === -1){
-    activePlayer = activePlayer === player1 ? player2 : player1;
-    alert(`${activePlayer.name}'s turn`);
+        gameboard[row][column] = activePlayer.symbol;
 
-    do {
-        row = prompt('Row: ');
-        column = prompt('Column: ');
-    } while (gameboard[row] == undefined || gameboard[row][column] == undefined || gameboard[row][column] !== 0)
+        console.table(gameboard);
+        gameStatus = checkVictory(gameboard);
 
-    gameboard[row][column] = activePlayer.symbol;
+        if(gameStatus === activePlayer.symbol){
+            alert(`${activePlayer.name} is victorious`);
+        }
 
-    console.table(gameboard);
-    gameStatus = checkVictory(gameboard);
-
-    if(gameStatus === activePlayer.symbol){
-        alert(`${activePlayer.name} is victorious`);
-    }
-
-    if (gameStatus === 0){
-        alert('draw');
+        if (gameStatus === 0){
+            alert('draw');
+        }
     }
 }
 
